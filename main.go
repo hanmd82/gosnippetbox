34438d94b5543fcbe89935c1e0ab029ffc5a1b4b
Mingding, Han <hanmd82@gmail.com>
2019-12-24 23:58:37 +0800

    feat: use servemux to serve HTTP routes

--- main.go
+++ main.go
@@ -8,7 +8,24 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from Snippetbox" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+    // Check if the current request URL path exactly matches "/". If it doesn't, use
+    // the http.NotFound() function to send a 404 response to the client, and return.
+    if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
     w.Write([]byte("Hello from Snippetbox"))
+}
+
+// Add a showSnippet handler function.
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([] byte("Display a specific snippet..."))
+}
+
+// Add a createSnippet handler function.
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+    w.Write([] byte("Create a new snippet..."))
 }
 
 func main() {
@@ -16,6 +33,10 @@ func main() {
     // register the home function as the handler for the "/" URL pattern.
     mux := http.NewServeMux()
     mux.HandleFunc("/", home)
+
+    // Register the two new handler functions and corresponding URL patterns with the servemux
+    mux.HandleFunc("/snippet", showSnippet)
+    mux.HandleFunc("/snippet/create", createSnippet)
 
     // Use the http.ListenAndServe() function to start a new web server, passing in
     // two parameters: the TCP network address to listen on (in this case ":4000")
