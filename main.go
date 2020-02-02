79ac3b420108eabf4161f530ad3c0cecd3919aac
Mingding, Han <hanmd82@gmail.com>
2020-02-02 15:48:29 +0800

    chore: Apply gofmt

--- main.go
+++ main.go
@@ -1,44 +1,44 @@
 package main
 
 import (
-    "log"
-    "net/http"
+	"log"
+	"net/http"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
-    if r.URL.Path != "/" {
-        http.NotFound(w, r)
-        return
-    }
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 
-    w.Write([]byte("Hello from Snippetbox"))
+	w.Write([]byte("Hello from Snippetbox"))
 }
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-    w.Write([] byte("Display a specific snippet..."))
+	w.Write([]byte("Display a specific snippet..."))
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-    if r.Method != http.MethodPost {
-        w.Header().Set("Allow", http.MethodPost)
-        http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed) // 405
-        // same as the following:
-        // w.WriteHeader(http.StatusMethodNotAllowed)
-        // w.Write([]byte("Method Not Allowed"))
-        return
-    }
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed) // 405
+		// same as the following:
+		// w.WriteHeader(http.StatusMethodNotAllowed)
+		// w.Write([]byte("Method Not Allowed"))
+		return
+	}
 
-    w.Write([] byte("Create a new snippet..."))
+	w.Write([]byte("Create a new snippet..."))
 }
 
 func main() {
-    mux := http.NewServeMux()
-    mux.HandleFunc("/", home)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
 
-    mux.HandleFunc("/snippet", showSnippet)
-    mux.HandleFunc("/snippet/create", createSnippet)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
-    log.Println("Starting server on :4000")
-    err := http.ListenAndServe(":4000", mux)
-    log.Fatal(err)
+	log.Println("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
